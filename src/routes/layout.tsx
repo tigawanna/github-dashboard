7894508c41737345e2c908a9a6b9686a892112ef
Dennis kinuthia <denniskinuthiawaweru@gmail.com>
2024-02-11 10:29:38 +0300

    hacky mobile sidebar

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,5 +1,11 @@
 import Nprogress from "@/components/navigation/nprogress/Nprogress";
-import { ClientSuspense, LayoutProps, useLocation,Head, PreloadContext } from "rakkasjs";
+import {
+  ClientSuspense,
+  LayoutProps,
+  useLocation,
+  Head,
+  PreloadContext,
+} from "rakkasjs";
 import "./index.css";
 import { Sidebar } from "@/components/navigation/bars/sidebar";
 import Toaster from "@/components/wrappers/DefaltExportedToaster";
@@ -10,7 +16,7 @@ import { Menu } from "lucide-react";
 
 function Layout({ children }: LayoutProps) {
   const location = useLocation();
-  const [open,setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
 
   return (
     <ErrorBoundaryComponent>
@@ -32,19 +38,27 @@ function Layout({ children }: LayoutProps) {
         </ClientSuspense>
         <div className="w-full flex gap-3">
           <div className="w-10 sm:hidden flex flex-col justify-center gap-3 relative ">
-            <Menu className="h-8 w-8 absolute top-[5%] left-[25%] z-40 " onClick={()=>setOpen(!open)}/>
-          {open&&<div className="sm:hidden w-full flex h-screen gap-2 animate-in fade-in zoom-in">
-            <Sidebar />
-          </div>}
-
-               </div>
+            <Menu
+              className="h-8 w-8 absolute top-[5%] left-[25%] z-40 "
+              onClick={() => setOpen(!open)}
+            />
+            {open && (
+              <div className="sm:hidden w-full flex h-screen gap-2 animate-in fade-in zoom-in">
+                <Sidebar />
+              </div>
+            )}
+          </div>
           <div className="hidden  min-w-[5%] w-fit sm:flex h-screen gap-2">
             <Sidebar />
           </div>
           <div className="w-full flex flex-col  gap-2 ">
-             {open&&<div className="sm:hidden fixed top-0 left-10 w-full h-screen z-40
-              animate-in fade-in zoom-in bg-base-300/60" onClick={()=>setOpen(!open)}>
-              </div>}
+            {open && (
+              <div
+                className="sm:hidden fixed top-0 left-10 w-full h-screen z-40
+              animate-in fade-in zoom-in bg-base-300/60"
+                onClick={() => setOpen(!open)}
+              ></div>
+            )}
             <div className="w-fit flex rounded-xl">
               <ClientSuspense fallback={null}>
                 <BreadCrumbs />
@@ -62,14 +76,15 @@ function Layout({ children }: LayoutProps) {
     </ErrorBoundaryComponent>
   );
 }
-Layout.preload = async(ctx: PreloadContext) => {
-
+Layout.preload = async (ctx: PreloadContext) => {
   return {
     head: {
       title: "Github Dashboard",
-      "og:title":"Github dashboard",
-      description: "Github dashboard built with Rakkasjs (vite + SSR) + Relay + Github GraphQL API",
-      "og:description": "Github dashboard built with Rakkasjs (vite + SSR) + Relay + Github GraphQL API",
+      "og:title": "Github dashboard",
+      description:
+        "Github dashboard built with Rakkasjs (vite + SSR) + Relay + Github GraphQL API",
+      "og:description":
+        "Github dashboard built with Rakkasjs (vite + SSR) + Relay + Github GraphQL API",
       "og:image": "/og.jpeg",
     },
   };
