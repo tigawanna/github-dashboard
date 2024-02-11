9286b87035d1a83faf6c49fe71e2d01f68c0689b
Dennis kinuthia <denniskinuthiawaweru@gmail.com>
2024-02-11 18:34:56 +0300

    add authguardto index route

---
+++ src/routes/$guard.ts
@@ -0,0 +1,20 @@
+import { LookupHookResult, PageRouteGuardContext } from "rakkasjs";
+
+export function pageGuard(ctx: PageRouteGuardContext): LookupHookResult {
+  const user = ctx.locals.pb?.authStore?.model;
+  // console.log(" ==== user in viewer guard  === ", user);
+  if(ctx.url.pathname==="/auth"){
+    return true
+  }
+  const auth_url = new URL(ctx.url);
+  auth_url.pathname = "auth";
+  auth_url.searchParams.set("return_to", ctx.url.pathname);
+  // console.log("user in auth route  ====== ",user)
+  if (!user) {
+    return {
+      redirect: auth_url.toString(),
+    };
+  } else {
+    return true;
+  }
+}
