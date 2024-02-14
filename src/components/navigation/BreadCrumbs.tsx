9f6f6b3e8c94dd7ba7423f59268ad83cfdcfb326
Dennis kinuthia <denniskinuthiawaweru@gmail.com>
2024-02-14 08:43:25 +0300

    scoot over bread crumbs

--- src/components/navigation/BreadCrumbs.tsx
+++ src/components/navigation/BreadCrumbs.tsx
@@ -8,7 +8,7 @@ export default function BreadCrumbs({}: BreadCrumbsProps) {
   const { breadcrumb_routes, current } = useRakkasBreadCrumbs();
 
   return (
-    <div className="flex flex-wrap z-50 px-1 gap-1">
+    <div className="flex flex-wrap z-50 px-1 py-2 ml-3 gap-1">
       {breadcrumb_routes.map(({ name, path }, idx) => {
         const new_url = new URL(current);
         new_url.pathname = path;
