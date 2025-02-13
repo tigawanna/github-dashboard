a9be04d513cc5888d19d894f1bc4175f1790abbe
tigawanna <denniskinuthiawaweru@gmail.com>
2025-02-13 07:35:00 +0300

    add GraphQL mutations for adding and removing stars from repositories

---
+++ src/routes/$user/repositories/-components/repo-card/__generated__/StarRepositoryAddStarMutation.graphql.ts
@@ -0,0 +1,165 @@
+/**
+ * @generated SignedSource<<aac682b22018c472ca69aa7f8dea3dab>>
+ * @lightSyntaxTransform
+ * @nogrep
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+// @ts-nocheck
+
+import { ConcreteRequest } from 'relay-runtime';
+export type StarRepositoryAddStarMutation$variables = {
+  starrableId: string;
+};
+export type StarRepositoryAddStarMutation$data = {
+  readonly addStar: {
+    readonly starrable: {
+      readonly id: string;
+      readonly stargazerCount: number;
+      readonly viewerHasStarred: boolean;
+    } | null | undefined;
+  } | null | undefined;
+};
+export type StarRepositoryAddStarMutation = {
+  response: StarRepositoryAddStarMutation$data;
+  variables: StarRepositoryAddStarMutation$variables;
+};
+
+const node: ConcreteRequest = (function(){
+var v0 = [
+  {
+    "defaultValue": null,
+    "kind": "LocalArgument",
+    "name": "starrableId"
+  }
+],
+v1 = [
+  {
+    "fields": [
+      {
+        "kind": "Literal",
+        "name": "clientMutationId",
+        "value": ""
+      },
+      {
+        "kind": "Variable",
+        "name": "starrableId",
+        "variableName": "starrableId"
+      }
+    ],
+    "kind": "ObjectValue",
+    "name": "input"
+  }
+],
+v2 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "id",
+  "storageKey": null
+},
+v3 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "stargazerCount",
+  "storageKey": null
+},
+v4 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "viewerHasStarred",
+  "storageKey": null
+};
+return {
+  "fragment": {
+    "argumentDefinitions": (v0/*: any*/),
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "StarRepositoryAddStarMutation",
+    "selections": [
+      {
+        "alias": null,
+        "args": (v1/*: any*/),
+        "concreteType": "AddStarPayload",
+        "kind": "LinkedField",
+        "name": "addStar",
+        "plural": false,
+        "selections": [
+          {
+            "alias": null,
+            "args": null,
+            "concreteType": null,
+            "kind": "LinkedField",
+            "name": "starrable",
+            "plural": false,
+            "selections": [
+              (v2/*: any*/),
+              (v3/*: any*/),
+              (v4/*: any*/)
+            ],
+            "storageKey": null
+          }
+        ],
+        "storageKey": null
+      }
+    ],
+    "type": "Mutation",
+    "abstractKey": null
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": (v0/*: any*/),
+    "kind": "Operation",
+    "name": "StarRepositoryAddStarMutation",
+    "selections": [
+      {
+        "alias": null,
+        "args": (v1/*: any*/),
+        "concreteType": "AddStarPayload",
+        "kind": "LinkedField",
+        "name": "addStar",
+        "plural": false,
+        "selections": [
+          {
+            "alias": null,
+            "args": null,
+            "concreteType": null,
+            "kind": "LinkedField",
+            "name": "starrable",
+            "plural": false,
+            "selections": [
+              {
+                "alias": null,
+                "args": null,
+                "kind": "ScalarField",
+                "name": "__typename",
+                "storageKey": null
+              },
+              (v2/*: any*/),
+              (v3/*: any*/),
+              (v4/*: any*/)
+            ],
+            "storageKey": null
+          }
+        ],
+        "storageKey": null
+      }
+    ]
+  },
+  "params": {
+    "cacheID": "dca511428eb2f45c662e2c5b2f64666f",
+    "id": null,
+    "metadata": {},
+    "name": "StarRepositoryAddStarMutation",
+    "operationKind": "mutation",
+    "text": "mutation StarRepositoryAddStarMutation(\n  $starrableId: ID!\n) {\n  addStar(input: {starrableId: $starrableId, clientMutationId: \"\"}) {\n    starrable {\n      __typename\n      id\n      stargazerCount\n      viewerHasStarred\n    }\n  }\n}\n"
+  }
+};
+})();
+
+(node as any).hash = "0cd4bf831085d4fd61b6ec9986033a6e";
+
+export default node;
