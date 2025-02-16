6c6fab0868164268dc2fffd66c9600444cb722f9
tigawanna <denniskinuthiawaweru@gmail.com>
2025-02-16 22:49:18 +0300

    add Repos_repositories fragment: implement GraphQL fragment for repositories data structure and pagination handling

---
+++ src/routes/repos/-components/__generated__/Repos_repositories.graphql.ts
@@ -0,0 +1,224 @@
+/**
+ * @generated SignedSource<<73462c2cc660b01c6ed210555d58299c>>
+ * @lightSyntaxTransform
+ * @nogrep
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+// @ts-nocheck
+
+import { ReaderFragment } from 'relay-runtime';
+import { FragmentRefs } from "relay-runtime";
+export type Repos_repositories$data = {
+  readonly id: string;
+  readonly repositories: {
+    readonly edges: ReadonlyArray<{
+      readonly cursor: string;
+      readonly node: {
+        readonly " $fragmentSpreads": FragmentRefs<"RepoCarde_reposiotory">;
+      } | null | undefined;
+    } | null | undefined> | null | undefined;
+    readonly pageInfo: {
+      readonly endCursor: string | null | undefined;
+      readonly hasNextPage: boolean;
+      readonly hasPreviousPage: boolean;
+      readonly startCursor: string | null | undefined;
+    };
+    readonly totalCount: number;
+  };
+  readonly " $fragmentType": "Repos_repositories";
+};
+export type Repos_repositories$key = {
+  readonly " $data"?: Repos_repositories$data;
+  readonly " $fragmentSpreads": FragmentRefs<"Repos_repositories">;
+};
+
+import ReposPaginationQuery_graphql from './ReposPaginationQuery.graphql';
+
+const node: ReaderFragment = (function(){
+var v0 = [
+  "repositories"
+];
+return {
+  "argumentDefinitions": [
+    {
+      "defaultValue": null,
+      "kind": "LocalArgument",
+      "name": "after"
+    },
+    {
+      "defaultValue": 2,
+      "kind": "LocalArgument",
+      "name": "first"
+    },
+    {
+      "defaultValue": false,
+      "kind": "LocalArgument",
+      "name": "isFork"
+    },
+    {
+      "defaultValue": {
+        "direction": "DESC",
+        "field": "PUSHED_AT"
+      },
+      "kind": "LocalArgument",
+      "name": "orderBy"
+    }
+  ],
+  "kind": "Fragment",
+  "metadata": {
+    "connection": [
+      {
+        "count": "first",
+        "cursor": "after",
+        "direction": "forward",
+        "path": (v0/*: any*/)
+      }
+    ],
+    "refetch": {
+      "connection": {
+        "forward": {
+          "count": "first",
+          "cursor": "after"
+        },
+        "backward": null,
+        "path": (v0/*: any*/)
+      },
+      "fragmentPathInResult": [
+        "node"
+      ],
+      "operation": ReposPaginationQuery_graphql,
+      "identifierInfo": {
+        "identifierField": "id",
+        "identifierQueryVariableName": "id"
+      }
+    }
+  },
+  "name": "Repos_repositories",
+  "selections": [
+    {
+      "alias": "repositories",
+      "args": [
+        {
+          "kind": "Variable",
+          "name": "isFork",
+          "variableName": "isFork"
+        },
+        {
+          "kind": "Variable",
+          "name": "orderBy",
+          "variableName": "orderBy"
+        }
+      ],
+      "concreteType": "RepositoryConnection",
+      "kind": "LinkedField",
+      "name": "__Repos_repositories_connection",
+      "plural": false,
+      "selections": [
+        {
+          "alias": null,
+          "args": null,
+          "concreteType": "RepositoryEdge",
+          "kind": "LinkedField",
+          "name": "edges",
+          "plural": true,
+          "selections": [
+            {
+              "alias": null,
+              "args": null,
+              "kind": "ScalarField",
+              "name": "cursor",
+              "storageKey": null
+            },
+            {
+              "alias": null,
+              "args": null,
+              "concreteType": "Repository",
+              "kind": "LinkedField",
+              "name": "node",
+              "plural": false,
+              "selections": [
+                {
+                  "args": null,
+                  "kind": "FragmentSpread",
+                  "name": "RepoCarde_reposiotory"
+                },
+                {
+                  "alias": null,
+                  "args": null,
+                  "kind": "ScalarField",
+                  "name": "__typename",
+                  "storageKey": null
+                }
+              ],
+              "storageKey": null
+            }
+          ],
+          "storageKey": null
+        },
+        {
+          "alias": null,
+          "args": null,
+          "concreteType": "PageInfo",
+          "kind": "LinkedField",
+          "name": "pageInfo",
+          "plural": false,
+          "selections": [
+            {
+              "alias": null,
+              "args": null,
+              "kind": "ScalarField",
+              "name": "endCursor",
+              "storageKey": null
+            },
+            {
+              "alias": null,
+              "args": null,
+              "kind": "ScalarField",
+              "name": "hasNextPage",
+              "storageKey": null
+            },
+            {
+              "alias": null,
+              "args": null,
+              "kind": "ScalarField",
+              "name": "hasPreviousPage",
+              "storageKey": null
+            },
+            {
+              "alias": null,
+              "args": null,
+              "kind": "ScalarField",
+              "name": "startCursor",
+              "storageKey": null
+            }
+          ],
+          "storageKey": null
+        },
+        {
+          "alias": null,
+          "args": null,
+          "kind": "ScalarField",
+          "name": "totalCount",
+          "storageKey": null
+        }
+      ],
+      "storageKey": null
+    },
+    {
+      "alias": null,
+      "args": null,
+      "kind": "ScalarField",
+      "name": "id",
+      "storageKey": null
+    }
+  ],
+  "type": "User",
+  "abstractKey": null
+};
+})();
+
+(node as any).hash = "de9692fbbdbebfe267f0dc777d4eec47";
+
+export default node;
