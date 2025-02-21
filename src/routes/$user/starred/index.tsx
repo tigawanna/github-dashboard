9c0aba4dc261430bd066981630032084dc8b1a50
tigawanna <denniskinuthiawaweru@gmail.com>
2025-02-22 00:14:36 +0300

    feat: implement loader for starred repositories and enhance query handling

--- src/routes/$user/starred/index.tsx
+++ src/routes/$user/starred/index.tsx
@@ -1,7 +1,42 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { UserStarredReposPage } from './-components/UserStarredReposPage';
+import { graphql } from 'relay-runtime';
+import { loadQuery } from 'react-relay';
+import { starredUserPageLoaderQuery } from './__generated__/starredUserPageLoaderQuery.graphql';
 
 export const Route = createFileRoute("/$user/starred/")({
   component: UserStarredReposPage,
+     loaderDeps(opts) {
+       return {
+         starOrder: opts.search.starOrder
+       };
+     },
+      loader(ctx) {
+        const { user } = ctx.params;
+        const { starOrder } = ctx.deps;
+        const relayEnv = ctx.context.relayEnviroment!;
+        const queryReference = loadQuery<starredUserPageLoaderQuery>(
+          relayEnv,
+          userStarredReposQuery,
+          {
+            login: user,
+            starOrder: {
+              field: starOrder?.field || "STARRED_AT",
+              direction: starOrder?.direction || "DESC",
+            },
+          },
+          { fetchPolicy: "store-or-network" }
+        );
+        return queryReference;
+      },
 });
 
+export const userStarredReposQuery = graphql`
+  query starredUserPageLoaderQuery(
+    $login: String!, 
+    $starOrder: StarOrder) {
+    user(login: $login) {
+      ...UserStarredRepos_repositories @arguments(orderByStarredRepos: $starOrder)
+    }
+  }
+`;
